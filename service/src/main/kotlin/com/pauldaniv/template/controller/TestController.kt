d6f23ef1d0bd61a72c4f9e50b4a5b47b40a2b4b2
Paul <paulsegfault@gmail.com>
2020-08-30 14:45:34 +0300

    Fixed test controller, added test client properties

--- service/src/main/kotlin/com/pauldaniv/template/controller/TestController.kt
+++ service/src/main/kotlin/com/pauldaniv/template/controller/TestController.kt
@@ -4,16 +4,20 @@ import com.pauldaniv.template.api.TestOneService
 import com.pauldaniv.template.lib.second.consume.User
 import com.pauldaniv.template.request.TestOne
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Service
-class TestController(
-    private val testOneService: TestOneService
-) {
+@RestController
+@RequestMapping("/hello")
+class TestController(private val testOneService: TestOneService) {
+
+  @GetMapping
   fun getIt(): ResponseEntity<TestOne> {
     return ResponseEntity.ok(testOneService.getIt())
   }
 
+  @GetMapping("/world")
   fun getUser(): ResponseEntity<User> {
     return ResponseEntity.ok(User("Test", "aoeu"))
   }

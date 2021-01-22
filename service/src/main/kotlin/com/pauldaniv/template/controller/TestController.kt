4e2ab85b396886427eef7d5751e4806757b12ae5
pauldaniv <24229102+pauldaniv@users.noreply.github.com>
2021-01-22 23:41:03 +0200

    Update TestController.kt

--- service/src/main/kotlin/com/pauldaniv/template/controller/TestController.kt
+++ service/src/main/kotlin/com/pauldaniv/template/controller/TestController.kt
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/api")
@@ -19,6 +20,6 @@ class TestController(private val testOneService: TestOneService) {
 
   @GetMapping("/example")
   fun getExamples(): ResponseEntity<User> {
-    return ResponseEntity.ok(User("Test", "aoeu"))
+    return ResponseEntity.ok(User("Test", LocalDateTime.now().toString()))
   }
 }
